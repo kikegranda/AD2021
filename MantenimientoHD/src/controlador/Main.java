89710d64c68eb71ea3e86393b9d0580a3bd339cb
Enrique <e.garciagranda@gmail.com>
2020-11-02 21:03:33 +0100

    Upload 2

--- MantenimientoHD/src/controlador/Main.java
+++ MantenimientoHD/src/controlador/Main.java
@@ -13,12 +13,11 @@ public class Main {
 		try {
 			Logica l = new Logica();
 
-			File ruta = new File("C:\\Users\\mr_ca\\OneDrive\\Escritorio\\Carpeta");
+			File ruta = new File("carpeta pruebas\\carpeta pruebas");
 			HashMap<File, String> mapa = l.mapDiscoAlmacenamiento();
 			ArrayList<File> lista = l.listarFicherosRuta(ruta);
 			ArrayList<File> lista2;
-
-			// lista2 = l.buscarRepetidoRuta(ruta);
+			
 			lista2 = l.ordenarListaFicheros(lista, "extension");
 
 			for (File file : mapa.keySet()) {
@@ -31,12 +30,11 @@ public class Main {
 			System.out.println();
 
 			lista2 = l.buscarRepetidoRuta(ruta);
-			
+
 			for (File file2 : lista2) {
 				System.out.println(file2.getAbsolutePath());
 			}
-			// l.copiarFicheros(ruta, new
-			// File("C:\\Users\\mr_ca\\OneDrive\\Escritorio\\Nueva carpeta (256)"));
+			l.copiarFicheros(ruta, new File("carpeta pruebas\\carpeta pruebas copia"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
